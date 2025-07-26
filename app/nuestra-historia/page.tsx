74c764c83a53d0e54ab5b94aecd10c4e8c176cee
BautyRemondino2 <bauty.remondino@gmail.com>
2025-07-26 16:28:58 -0300

    nuestra historia

---
+++ app/nuestra-historia/page.tsx
@@ -0,0 +1,23 @@
+import '../ui/nuestra_historia.css';
+import { Cinzel, Open_Sans } from 'next/font/google';
+
+const cinzel = Cinzel({ subsets: ['latin'], weight: ['400', '700'] });
+const openSans = Open_Sans({ subsets: ['latin'], weight: ['400', '600'] });
+
+export default function NuestraHistoria() {
+  return (
+    <div className="min-h-screen w-full relative overflow-x-hidden">
+      <div className="historia-section">
+        <div className="historia-img">
+          <img src="/abue_papa.jpg" alt="Historia El Retiro" className="historia-img-content" />
+        </div>
+        <div className="historia-texto">
+          <h3 className={`${cinzel.className} nombre-fundador`}>Nuestra historia</h3>
+          <p>
+            Mi nombre es Ricardo J. Remondino y junto a mi padre fundamos “CAMPOS Y CABAÑA EL RETIRO S.A.” y desde hace 30 años nos dedicamos a criar y producir Genética BRAFORD de la más alta calidad para nuestro propio REMATE ANUAL y seguir participando en las más importantes exposiciones ganaderas, entre las que se cuenta la EXPOSICIÓN INTERNACIONAL DE PALERMO.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
