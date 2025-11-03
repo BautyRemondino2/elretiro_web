bbe28d399998dfa9afb9a4ce5e4a482a138528f9
BautyRemondino2 <bauty.remondino@gmail.com>
2025-11-03 09:30:46 -0300

    Ignorar errores TS temporales en Next 15

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    // ⚠️ Ignora los errores de compilación de TypeScript en el build.
+    // Esto es TEMPORAL, hasta que Next.js 15 solucione el bug con PageProps.
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
