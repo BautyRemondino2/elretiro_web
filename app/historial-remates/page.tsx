cc8e5e32360e7e9e063584ee717a830fa093e589
BautyRemondino2 <bauty.remondino@gmail.com>
2025-11-03 09:38:35 -0300

    catag

--- app/historial-remates/page.tsx
+++ app/historial-remates/page.tsx
@@ -16,7 +16,8 @@ export default function NuestraHistoria() {
     </p>
     <div className="galeria-catalogos">
         {[
-          { year: 2024, file: 'CATALOGO 2024_comprimido.pdf', preview: `/catalogos/catalogo_previews/CATALOGO_2024.jpg` },
+          { year: 2025, file: 'CATALOGO 2025.pdf', preview: `/catalogos/catalogo_previews/CATALOGO_2025.jpg` },
+          { year: 2024, file: 'CATALOGO 2024_compressed.pdf', preview: `/catalogos/catalogo_previews/CATALOGO_2024.jpg` },
           { year: 2023, file: 'CATALOGO 2023-comprimido.pdf', preview: `/catalogos/catalogo_previews/CATALOGO_2023.jpg` },
           { year: 2022, file: 'CATALOGO 2022_compressed.pdf', preview: `/catalogos/catalogo_previews/CATALOGO_2022.jpg` },
           { year: 2021, file: 'CATALOGO 2021.pdf', preview: `/catalogos/catalogo_previews/CATALOGO_2021.jpg` },
